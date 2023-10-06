e4e481826a41aeedbff7e7174ba01e840aaf9a76
Sudeep Ratnaparkhe <sudeeppr1998@gmail.com>
2023-10-06 09:13:41 +0530

    Mean median api's added and ai4bharat api added

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,25 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { fastifyMultipart } from '@fastify/multipart';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule.forRoot(process.env.DATABASE),
     new FastifyAdapter({ logger: true }),
   );
+
+  await app.register(fastifyMultipart, { attachFieldsToBody: 'keyValues' });
+
+  const config = new DocumentBuilder().setTitle('ALL Learner AI')
+    .setDescription("ALL Learner AI API Spec Documentation")
+    .setVersion('v1')
+    .addTag('scores')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors({
     origin: ["*"],
